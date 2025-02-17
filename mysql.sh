cdaa17ca407919184aa6f342dbfadf6a6ce97552
samarareddy <samara.junna@gmail.com>
2025-02-17 14:22:53 +0530

    some-message

--- mysql.sh
+++ mysql.sh
@@ -18,10 +18,10 @@ VALIDATE $? "starting mysql server"
 # mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFI
 # VALIDATE $? "setting mysql root password"
 
-mysql -h 172.31.84.44 -u root -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
+mysql -h db.mydaws.online -u root -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
 if [ $? -ne 0 ]
 then
-    mysql_secure_installation --set-root-pass ${mysql_root_passwor
+    mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
     VALIDATE $? "setting mysql root password"
 else
     echo -e "mysql root password is already setup...$Y SKIPPING $N
