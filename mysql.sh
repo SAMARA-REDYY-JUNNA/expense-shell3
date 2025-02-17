28416e4be05cc49b60603068c741d0414ce0d25a
samarareddy <samara.junna@gmail.com>
2025-02-17 14:02:12 +0530

    some-message

--- mysql.sh
+++ mysql.sh
@@ -18,7 +18,7 @@ VALIDATE $? "starting mysql server"
 # mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFI
 # VALIDATE $? "setting mysql root password"
 
-mysql -h db.mydaws.online -u root -p${mysql_root_password} -e 'sho
+mysql -h db.mydaws.online -u root -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
 if [ $? -ne 0 ]
 then
     mysql_secure_installation --set-root-pass ${mysql_root_passwor
