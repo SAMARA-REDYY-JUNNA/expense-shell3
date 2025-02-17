93c9d1b4f2a881861f2fc47464ed9e0fba622f5b
samarareddy <samara.junna@gmail.com>
2025-02-17 11:53:17 +0530

    some-message

---
+++ mysql.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+source ./common.sh
+
+check_root()
+
+echo "please enter DB password:"
+read -s mysql_root_password
+
+dnf install mysql-server -y &>>$LOGFILE
+VALIDATE $? "installing mysql server"
+
+systemctl enable mysqld &>>$LOGFILE
+VALIDATE $? "enabling mysql server"
+
+systemctl start mysqld &>>$LOGFILE
+VALIDATE $? "starting mysql server"
+
+# mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
+# VALIDATE $? "setting mysql root password"
+
+mysql -h db.mydaws.online -u root -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
+if [ $? -ne 0 ]
+then
+    mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
+    VALIDATE $? "setting mysql root password"
+else
+    echo -e "mysql root password is already setup...$Y SKIPPING $N"
+fi    
